85b4353369e22ab8b114bcf7d18c16ae73dcf2fe
Anshu Kumar <110124018@nitt.edu>
2025-07-16 19:11:59 +0530

    Update page.js

--- app/new/page.js
+++ app/new/page.js
@@ -142,7 +142,7 @@ const page = () => {
         <div className="body max-w-[90vw] overflow-y-auto min-h-[110vh] bg-white mx-auto mt-10 rounded-2xl bg-gradient-to-l from-white to-gray-500 pt-3">
           <button onClick={() => {
             setshowform(true)
-          }} className=' rounded-sm p-2 text-3xl text-center flex items-center justify-center mx-auto bg-green-600  text-black hover:bg-green-800 '><p className='cursor-pointer'>Add new Team</p></button>
+          }} className=' rounded-sm p-2 text-3xl text-center flex items-center justify-center mx-auto bg-blue-600  text-black hover:bg-blue-800 '><p className='cursor-pointer'>Add new Team</p></button>
           {data.length > 0 &&
 
             data.map((e, i) => (<>
