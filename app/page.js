b936995239275225c8625290db20de18e309c86c
Anshu Kumar <110124018@nitt.edu>
2025-07-18 22:05:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
 <div className="flex  min-w-[100vw] min-h-[100vh]   bg-black  bg-no-repeat bg-cover bg-center h-screen custom-font  " >
 <div>
 
-  <img src="/bgtc_upscaled.jpg" alt="NITTeamly Logo" className="absolute top-10 left-10 w-full " />
+  <img src="/bgtc_upscaled.jpg" alt="NITTeamly Logo" className="absolute top-40 left-10 w-full " />
   <p className=" text-2xl md:text-4xl lg:text-6xl  text-white flex flex-col  custom-font">
     <span className="text-4xl md:text-6xl lg:text-8xl custom-font mt-20 ml-[23vw] ">Welcome to NITTeamly, </span>
     <span className=" mt-30 ml-10 md:ml-30 lg:ml-90 absolute bottom-[-110]">From 
