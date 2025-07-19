42614baf7b52e2b6b29df27283da4bb0866ecab2
Anshu Kumar <110124018@nitt.edu>
2025-07-19 10:48:25 +0530

    responsive

--- app/components/Footer/footer.js
+++ app/components/Footer/footer.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   <footer className="bg-black text-white py-6 mt-10">
+   <footer className="bg-black text-white py-6 mt-10 relative z-10  top-[-40vh] md:top-[-15vh] lg:top-[10vh]">
   <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
     
     <div className="text-center md:text-left mb-4 md:mb-0">
-      <h1 className="text-[3vw] lg:text-2xl font-bold text-blue-400">NITTeamly</h1>
-      <p className="text-[2vw] lg:text-sm text-gray-400">Connecting Trichians, one group at a time.</p>
+      <h1 className="text-2xl font-bold text-blue-400">NITTeamly</h1>
+      <p className="text-sm text-gray-400">Connecting Trichians, one group at a time.</p>
     </div>
 
-    <ul className="flex space-x-6 text-[2vw] lg:text-sm text-gray-300">
+    <ul className="flex space-x-6 text-sm text-gray-300">
       <li><a href="/about" className="hover:text-white transition">About</a></li>
       <li><a href="/contact" className="hover:text-white transition">Contact</a></li>
       <li><a href="/privacy" className="hover:text-white transition">Privacy</a></li>
     </ul>
   </div>
 
-  <div className=" text-center text-gray-500 text-[1vw] lg:text-sm">
+  <div className=" text-center text-gray-500 ">
     Made with ❤️ by Team TC · © {new Date().getFullYear()} NIT Trichy
   </div>
 </footer>
--- app/page.js
+++ app/page.js
@@ -22,12 +22,14 @@ export default function Home() {
   href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@400..800&family=Bebas+Neue&family=Orbitron:wght@400..900&display=swap"
   rel="stylesheet"
 />
-<div className="flex  min-w-[100vw] min-h-[100vh] bg-[url('/smallbg.png')]   lg:bg-[url('/bgtc_upscaled.jpg')]   bg-no-repeat bg-cover bg-center h-screen custom-font  " >
+<div className="flex  min-w-[100vw] max-h-[100vh]    bg-black h-screen custom-font  " >
+<img src="/bgtc_upscaled.jpg" className="absolute  mx-auto  h-[60vw] mt-[10vh] ml-[1vw] " />
 <div className="mx-auto">
+
   
   <p className=" text-2xl md:text-4xl lg:text-6xl  text-white flex flex-col  custom-font">
-    <span className="text-4xl md:text-6xl lg:text-8xl custom-font mt-20 mx-auto ">Welcome to NITTeamly, </span>
-    <span className=" mt-30  mx-auto absolute bottom-[-110]">From 
+    <span className="text-4xl md:text-6xl lg:text-8xl custom-font mt-20 mx-auto relative z-10 ">Welcome to NITTeamly, </span>
+    <span className=" mt-30  mx-auto relative top-[20vh] md:top-[34vh] lg:top-[70vh] ">From 
       <span className="text-blue-700 font-bold"> Strangers  
         </span> to Teammates — Instantly.</span>
         </p>
