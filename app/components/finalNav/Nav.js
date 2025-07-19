37e8052b39ccad8952eda0f5fb0432cb11621e8f
Anshu Kumar <110124018@nitt.edu>
2025-07-19 15:58:55 +0530

    responsiveness

--- app/components/finalNav/Nav.js
+++ app/components/finalNav/Nav.js
@@ -4,8 +4,8 @@ const FNavbar = () => {
   return (
     <div className='bg-gradient-to-r from-blue-800 to-black text-white  p-4 '>
       <ul className='flex items-center justify-between '>
-        <li className='flex items-center justify-center'><img className='w-[3vw]' src="/wired-outline-955-demand-hover-roll.gif"/></li>
-        <li className='text-4xl font-semibold'>NITTeamly- Create Or Join a Team</li>
+        <li className='flex items-center justify-center'><img className=' w-[10vw] lg:w-[3vw]' src="/wired-outline-955-demand-hover-roll.gif"/></li>
+        <li className='text-lg md:text-2xl lg:text-4xl font-semibold'>NITTeamly- Create Or Join a Team</li>
         
 
       </ul>
